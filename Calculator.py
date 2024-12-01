12ee42243f2fdcfa6f1e23114711e80113afe742
ChumaSuey <hyperghostblade@gmail.com>
2024-12-01 10:08:22 -0400

    Comments improved.

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 from tkinter import messagebox
+# In one of my jobs i do administrative tasks, one of them is counting the tolls of our vehicles
+# Each toll is a different amount, and each vehicle has a different quantity of peajes(travels)
+# This is a multiplicative operation, so we can calculate the total tolls as a sum of all tolls * quantity
 
+# Global variables that will work as lists
+# Montos will be the tolls, and cantidades will be quantities of travels for each toll with that price.
 montos = []
 cantidades = []
-
+# Add new toll fields (function)
 def add_toll_fields():
     new_label_monto = tk.Label(root, text="Monto:")
     new_label_monto.pack()
@@ -17,16 +22,16 @@ def add_toll_fields():
 
     montos.append(new_entry_monto)
     cantidades.append(new_entry_cantidad)
-
+# Calculate the total tolls (function)
 def calculate_total_tolls():
-    # Check if any of the values in montos and cantidades are empty
+    # Check if any of the values in montos and cantidades are empty (validation)
     if any(not entry_monto.get() or not entry_cantidad.get() for entry_monto, entry_cantidad in zip(montos, cantidades)):
         messagebox.showwarning("Empty values", "Please enter values for all tolls and quantities.")
         return
-
+    # Calculate the total tolls
     total = sum(float(monto.get()) * int(cantidad.get()) for monto, cantidad in zip(montos, cantidades))
     messagebox.showinfo("Total de peajes", f"El total de peajes es: {total}")
-
+# Reset the fields (function)
 def reset_fields():
     for entry_monto in montos:
         entry_monto.delete(0, tk.END)
@@ -45,6 +50,8 @@ def reset_fields():
     # Add a new toll field (with default values)
     add_toll_fields()
 
+# All buttons have a root bind so that they work with the keyboard
+# Also within the Text labels, between [ ] is the keyboard shortcut indicated
 # Create the main window
 root = tk.Tk()
 root.title("Calculadora de peajes")
@@ -71,4 +78,4 @@ root.bind("<w>", lambda event: reset_fields())
 if __name__ == "__main__":
     root.mainloop()
 
-# Notes: The reset button just cleans the numbers / strings, it doesn't remove the toll and quantity fields
+# Notes: The reset button makes the toll/monto to be a cleaned single entry.
